65acb9b297db1d8b49f0ef0d44fe6fd6fed0af65
Mr.CHOO <76519461+kevin0591@users.noreply.github.com>
2024-04-04 16:14:23 +0900

    Revert "Updated : view.java -> added mainMenu"

--- parking-lot-system/src/main/java/parkingsystem/controller/ParkingController.java
+++ parking-lot-system/src/main/java/parkingsystem/controller/ParkingController.java
@@ -1,33 +1,21 @@
 package parkingsystem.controller;
 
 import parkingsystem.model.dto.Car;
-import parkingsystem.model.dto.ParkingSpace;
 import parkingsystem.repos.ParkingLot;
 
 public class ParkingController {
 
     public ParkingLot pl = new ParkingLot(30, 10);
 
-    CarEntry carEntry = new CarEntry(pl);
+    CarEntry carEntry = new CarEntry();
     CarExit carExit = new CarExit();
 
     // 입차, 출차
-    public void entry() {
-        carEntry.parkVehicle();
-
-        for (ParkingSpace i : pl.getParkingLot()) {
-            System.out.println(i);
-        }
-
-        System.out.println("================================");
-        for(ParkingSpace i : pl.getParkingTower()) {
-            System.out.println(i);
-        }
+    public void entry(ParkingLot space, Car car) {
+        new CarEntry();
     }
 
-
-
-    public void exit (ParkingLot space, Car car){
+    public void exit(ParkingLot space, Car car) {
         new CarExit();
     }
 
