b7b787e215f82277deffde5e868336f1873cb448
Gyunorld <mingue640@gmail.com>
2024-04-04 18:11:14 +0900

    CarExit추가

--- parking-lot-system/src/main/java/parkingsystem/controller/CarExit.java
+++ parking-lot-system/src/main/java/parkingsystem/controller/CarExit.java
@@ -1,8 +1,55 @@
 package parkingsystem.controller;
 
+import parkingsystem.model.dto.ParkingSpace;
+import parkingsystem.repos.ParkingLot;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CarExit {
-    
+
     /*
     출차 메소드, 컨트롤러에 생성된 parking space를 인자로 받아서 처리..?
+
+    1. 사용자로부터 차량 번호를 입력받기
+    2. 입력받은 차량 번호를 이용하여 차량을 찾기
+    3-1. 찾은 차량을 초기화한다.
+    3-2. 주차자리
      */
+
+
+    private ParkingLot parkingLot;
+
+    public CarExit() {
+    }
+
+    public CarExit(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
+    }
+
+
+    public void vehicleOut() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("출차할 차량 번호를 입력하세요: ");
+        int PlateNum = sc.nextInt();
+
+        ArrayList<ParkingSpace> pg  = parkingLot.getParkingLot();
+        ArrayList<ParkingSpace> pt  = parkingLot.getParkingTower();
+
+
+        for (int i = 0; i < pg.size(); i++){
+            if (PlateNum == pg.get(i).getParkedCar().getCarNum()) {
+                pg.get(i).setParkedCar(null);
+            }
+        }
+        for (int i = 0; i < pt.size(); i++){
+            if (PlateNum == pg.get(i).getParkedCar().getCarNum()) {
+                pt.get(i).setParkedCar(null);
+            } else {
+                System.out.println("해당 차량은 없습니다.");
+            }
+        }
+
+    }
+
 }
--- parking-lot-system/src/main/java/parkingsystem/controller/ParkingController.java
+++ parking-lot-system/src/main/java/parkingsystem/controller/ParkingController.java
@@ -9,7 +9,7 @@ public class ParkingController {
     public ParkingLot pl = new ParkingLot(10, 5);
 
     CarEntry carEntry = new CarEntry(pl);
-    CarExit carExit = new CarExit();
+    CarExit carExit = new CarExit(pl);
 
     // 입차, 출차
     public void entry() {
@@ -17,6 +17,7 @@ public class ParkingController {
         for(ParkingSpace i : pl.getParkingLot()){
             System.out.println(i);
         }
+        System.out.println("----------------------------");
         for(ParkingSpace i : pl.getParkingTower()) {
             System.out.println(i);
         }
